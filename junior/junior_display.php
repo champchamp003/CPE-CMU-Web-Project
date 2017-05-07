5cd721172ac9c6afdfcacb8cbf753b94dd805438
KjN <kanin.sukarote@gmail.com>
2017-05-07 17:35:49 +0700

    4students

---
+++ junior/junior_display.php
@@ -0,0 +1,52 @@
+
+<?php
+
+	$servername = "localhost";
+	$username = "root";
+	$password = "";
+	$dbname = "phpbasic";
+	$conn = new mysqli($servername, $username, $password,$dbname);
+	mysqli_set_charset($conn, "utf8");
+
+	// Check connection
+	if ($conn->connect_error) {
+		die("Connection failed: " . $conn->connect_error);
+	} 
+	
+  $sql = "SELECT * FROM junior where id='".$_GET["id"]."'";
+  $result = $conn->query($sql);
+  $row = $result->fetch_assoc()
+	
+	
+?>
+    <!-- MAIN SECTION -->
+    <section class="mainContent full-width clearfix">
+      <div class="container">
+            <div class="row">
+                  <div class="col-sm-4 col-xs-12">
+                    <div class="juniorPhoto">
+                      <img src="<?=$row["img"]?>" alt="img class="img-rounded img-responsive">
+                    </div>
+                  </div>
+                  <div class="col-sm-8 col-xs-12">
+                    <div class="juniorInfo">
+                          <div class="juniorProfession bg-color-3">รหัสนักศึกษา</div>
+                          <div class="professionDetails"><?=$row["student_id"]?></div>
+                          <h3><?=$row["firstname"]?> <?=$row["lastname"]?></h3>
+                          <div class="juniorProfession bg-color-3">ชื่อเล่น</div>
+                          <div class="professionDetails"><?=$row["nickname"]?></div>
+                          <div class="juniorProfession bg-color-4">เพศ</div>
+                          <div class="professionDetails"><?=$row["gender"]?></div>
+                          <div class="juniorProfession bg-color-6">อายุ</div>
+                          <div class="professionDetails"><?=$row["age"]?></div>
+                          <div class="juniorProfession bg-color-7">โรงเรียน</div>
+                          <div class="professionDetails"><?=$row["school"]?></div>
+                          <div class="juniorProfession bg-color-8">เข้าศึกษา</div>
+                          <div class="professionDetails"><?=$row["entrane"]?></div>
+                    </div>
+                  </div>
+            </div>
+      </div>
+    </section>
+<?php $conn->close(); ?>
+    
---
+++ junior/junior_form.php
@@ -0,0 +1,82 @@
+    <!-- MAIN SECTION -->
+     <section class="mainContent full-width clearfix">
+     <div class="container">
+        <div class="row">
+             <div class="col-xs-offset-1 col-xs-10">
+                <div class="panel panel-default formPanel">
+                          <div class="panel-heading bg-color-1 border-color-1">
+                            <h3 class="panel-title">Create  an account</h3>
+                          </div>
+                          <div class="panel-body">
+                                <form action="junior_add.php" method="POST" role="form" enctype="multipart/form-data">
+                                      <div class="form-group formField">
+                                        <div class="col-xs-12">
+                                           <label for="">รหัสนักศึกษา</label>
+                                           <input type="text" class="form-control" name="student_id" placeholder="รหัานักศึกษา" required>
+                                        </div>
+                                      </div>
+                                      <div class="form-group formField">
+                                        <div class="col-xs-12">
+                                           <label for="">ชื่อ</label>
+                                           <input type="text" class="form-control" name="firstname" placeholder="ชื่อ" required>
+                                        </div>
+                                      </div>
+                                       <div class="form-group formField">
+                                        <div class="col-xs-12">
+                                              <label for="">นามสกุล</label>
+                                               <input type="text" class="form-control" name="lastname" placeholder="นามสกุล" required>
+                                        </div>
+                                      </div>
+                                      
+                                      <div class="form-group formField">
+                                        <div class="col-xs-12">
+                                              <label for="">ชื่อเล่น</label>
+                                               <input type="text" class="form-control" name="nickname" placeholder="ชื่อเล่น" required>
+                                        </div>
+
+                                        <div class="form-group formField">
+                                        <div class="col-xs-12">
+                                              <label for="">เพศ</label>
+                                               <input type="text" class="form-control" name="geืder" placeholder="เพศ" required>
+                                        </div>
+                                        <div class="form-group formField">
+                                        <div class="col-xs-12">
+                                           <label for="">อายุ</label>
+                                           <input type="text" class="form-control" name="age" placeholder="อายุ" required>
+                                        </div>
+                                      </div>
+                                      <div class="form-group formField">
+                                        <div class="col-xs-12">
+                                           <label for="">จบจากโรงเรียน</label>
+                                           <input type="text" class="form-control" name="school" placeholder="จบจากโรงเรียน" required>
+                                        </div>
+                                      </div>
+                                      <div class="form-group formField">
+                                        <div class="col-xs-12">
+                                           <label for="">การเข้าศึษา</label>
+                                           <input type="text" class="form-control" name="entrence" placeholder="การเข้าศึกษา" required>
+                                        </div>
+                                      </div>
+
+                                        <div class="form-group">
+                                        <div class="col-xs-12">
+                                              <label for="">รูปภาพ</label>
+                                              <input type="file" name="img" id="img" required>
+                                            </div>
+
+                                        </div>
+                                      <div class="form-group formField">
+                                            <div class="col-xs-12">
+                                            	<label></label>
+                                                <input type="submit" class="btn btn-primary btn-block bg-color-3 border-color-3" value="Submit">
+                                            </div>
+                                      </div>
+                                </form>
+             			 </div>
+            		</div>
+             </div>
+        </div>
+          
+      </div>
+    </section>
+    
