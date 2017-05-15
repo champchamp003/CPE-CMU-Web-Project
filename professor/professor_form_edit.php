29cf287ea02dc82e67e040fc9ab6340375ec8d17
champchamp003 <warakornchamp@gmail.com>
2017-05-15 14:33:04 +0700

    edit 1.02

--- professor/professor_form_edit.php
+++ professor/professor_form_edit.php
@@ -91,32 +91,32 @@
                               <div class="form-group formField">
                                 <div class="col-xs-12">
                                    <label for="">ชื่อ</label>
-                                   <input type="text" class="form-control" name="firstname" value="<?=$row["firstname"]?>" >
+                                   <input type="text" class="form-control" name="firstname" value="<?=$row["firstname"]?>" required>
                                </div>
                            </div>
                            <div class="form-group formField">
                               <div class="col-xs-12">
                                 <label for="">นามสกุล</label>
-                                <input type="text" class="form-control" name="lastname" value="<?=$row["lastname"]?>" >
+                                <input type="text" class="form-control" name="lastname" value="<?=$row["lastname"]?>" required>
                             </div>
                         </div>
 
                         <div class="form-group formField">
                           <div class="col-xs-12">
                             <label for="">ตำแหน่ง</label>
-                            <input type="text" class="form-control" name="position" value="<?=$row["position"]?>" >
+                            <input type="text" class="form-control" name="position" value="<?=$row["position"]?>" required>
                         </div>
 
                         <div class="form-group formField">
                             <div class="col-xs-12">
                               <label for="">จบจากมหาวิทยาลัยอะไร ?</label>
-                              <input type="text" class="form-control" name="university" value="<?=$row["university"]?>" >
+                              <input type="text" class="form-control" name="university" value="<?=$row["university"]?>" required>
                           </div>
 
                           <div class="form-group">
                               <div class="col-xs-12">
                                 <label for="">ความสนใจพิเศษ</label>
-                                <textarea class="form-control" name="specialist"  rows="10"><?=$row["specialist"]?></textarea>
+                                <textarea class="form-control" name="specialist"  rows="10" required ><?=$row["specialist"]?></textarea>
                             </div>
 
                         </div>
